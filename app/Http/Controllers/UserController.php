c32c230f7f96cf54cdddf191db99f2f979a630b3
KIRAGGA <preagkan@gmail.com>
2020-05-16 23:13:07 +0300

    highlight of model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,13 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\User;
 
 class UserController extends Controller
 {
     public function index()
     {
+        // DB::insert('insert into users (name, email,password) values (?,?,?)',
+        // [
+        //     'reagan','reagan@gmail.com','password',
+        // ]);
+        // $users =DB::select('select * from users');
+        // return $users;
+
+        $usr = new User();
+        dd($usr);
         return view('home');
     }
 }
