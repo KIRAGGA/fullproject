c4f389f072667ddbebc114ba5d532cda011a7a2b
KIRAGGA <preagkan@gmail.com>
2020-06-02 22:14:23 +0300

    working on the todos

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function(){
 
+    return view('todos.index');
+});
 Route::get('/', function () {
     return view('welcome');
         });
