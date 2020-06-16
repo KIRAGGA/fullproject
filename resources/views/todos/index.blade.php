0cc8836ac6c6780d4dc9000906c3377572c1f3be
KIRAGGA <preagkan@gmail.com>
2020-06-16 23:04:57 +0300

    working on todo

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -3,17 +3,29 @@
 @section('content')
 <div class="flex justify-center border-b pb-4">
         <h1 class="text-2x1">All Todo list</h1>
-        <a href="/todos/create" class="mx-5 py-1 px-1 cursor-pointer rounded text-white bg-blue-400">Create New</a>
+        <a href="/todos/create" class="mx-5 py-1 px-1 cursor-pointer rounded text-white bg-blue-400">
+            Create New</a>
     </div>
         <ul class="my-5">
         @foreach ($todos as $todo)
 
             <div class="flex justify-between border-b pb-4">
-                {{$todo->title}} 
+                <p>{{$todo->title}} </p>
+
+                <div>
+                    
                 <a href="{{'/todos/'.$todo->id.'/edit'}}" 
-                    class="bg-orange-400 cursor-pointer rounded text-white">
-                <span class="fas fa-edit px-2"></span>
+                    class="text-orange-400 cursor-pointer text-white">
+                        <span class="fas fa-edit px-2"></span>
                 </a>
+
+                @if($todo->completed)
+                     <span class="fas fa-check text-green-400 px-2"></span>
+                @else
+                    <span class="fas fa-check  text-gray-300  cursor-pointer px-2"></span>
+                @endif
+
+            </div>
                 {{-- @include('todos.completed-button') --}}
             </div>
 
