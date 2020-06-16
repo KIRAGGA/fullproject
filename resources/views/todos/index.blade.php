042235ac8210cb12a5bd915bdc6218901e91d5a0
KIRAGGA <preagkan@gmail.com>
2020-06-16 15:14:15 +0300

    Working on todos

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,8 +1,11 @@
+<p><a href="/todo/create" class="curso-pointer rounded text-white bg-blue-400">Create New</a></p>
+
 <ul>
 @foreach ($todos as $todo)
 
-<li>
-{{$todo->title}} 
+<li class="flex">
+<p>{{$todo->title}} <a href="/todos/edit" class="bg-orange-400 cursor-pointer rounded text-white">Edit</a></p>
+
 </li>
 @endforeach
 </ul>
