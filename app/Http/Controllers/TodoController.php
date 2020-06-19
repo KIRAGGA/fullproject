9aa8cf6ec887611bc606534c1a879bd11865141c
KIRAGGA <preagkan@gmail.com>
2020-06-19 13:29:05 +0300

    working on todo

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -29,7 +29,7 @@ class TodoController extends Controller
 
     public function show (Todo $todo)
     {
-        return view(todo.show);
+        return view('todos.show', compact('todo'));
     }
 
 
