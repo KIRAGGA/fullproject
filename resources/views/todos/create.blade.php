0668840ff5ee6cae34b10e5af9298bab18dce49a
KIRAGGA <preagkan@gmail.com>
2020-06-20 11:51:33 +0300

    working on todo

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -22,6 +22,17 @@
             <div class="py-1">
                 <textarea type="text" name="description" placeholder="Description" class="py-2 px-2 rounded border"></textarea>
             </div>
+
+            <div class="py-2">
+                <div class="flex justify-center pb-4 px-4">
+                    <h1 class="text-lg pb-4"> Add steps if required</h1>
+                 
+                <span class="fas fa-plus cursor-pointer px-2 py-1"></span>
+                </div>
+                <input type="text" name="step" placeholder="steps" class="py-2 px-2 
+                rounded border"/>
+            </div>
+
             <div class="py-1">
                 <input type="submit" value="create" class="p-2 border rounded">
             </div>
