fdf13c4e48259718d2bc3385da02f1017d0d8206
KIRAGGA <preagkan@gmail.com>
2020-06-20 13:31:37 +0300

    working on step livewire

--- resources/views/livewire/step.blade.php
+++ resources/views/livewire/step.blade.php
@@ -3,12 +3,17 @@
     <div class="py-2">
         <div class="flex justify-center pb-4 px-4">
             <h1 class="text-lg pb-4"> Add steps if required</h1>
-         
-        <span wire:click="increment" class="fas fa-plus cursor-pointer px-2 py-1"></span>
+         <span wire:click="increment" class="fas fa-plus cursor-pointer px-2 py-1"></span>
         </div>
-        <input type="text" name="step" placeholder="steps" class="py-2 px-2 
-        rounded border"/>
     </div>
 
-    {{$steps}}
+
+        @for ($i = 0; $i < count($steps); $i++)
+            <div class="flex justify-center py-2">
+                <input type="text" name="step" 
+                    placeholder="{{'Describe step '.($i+1)}}" class="py-1 px-2 rounded border"/>   
+                    
+                    <span class="fas fa-times text-red-400 p-2" wire:click="remove({{$i}})"></span>
+            </div>
+            @endfor
 </div>
