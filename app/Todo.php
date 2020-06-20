b2c673429adb3c314726e07aab19010bbd754b83
KIRAGGA <preagkan@gmail.com>
2020-06-20 14:07:01 +0300

    Working on steps

--- app/Todo.php
+++ app/Todo.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Step;
 class Todo extends Model
 {
     protected $fillable = ['title', 'completed', 'user_id', 'description'];
+
+
+    public function Steps()
+    {
+        return $this->hasMany(Step::class);
+    }
 }
